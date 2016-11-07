836159968afebede42305581141e048e379a0e61
Joseph Walsh <j.thomas.walsh@gmail.com>
2016-11-06 22:01:23 -0600

    structured directory, wrote first functions, added documentation

---
+++ R/get_fighters.R
@@ -0,0 +1,14 @@
+#' Get fighter from database
+#' 
+#' Returns a dataframe with fighter data
+#' 
+#' @param con Connection to Postgres database
+#' @param tablename The table in the database with fighter information
+#' @return A dataframe with a row for each fighter
+#' @export 
+
+query_fighters <- function(tablename){
+  query = paste("select * from ", tablename)
+  fighters <- RPostgreSQL::dbGetQuery(con, query)
+  return(fighters)
+}
